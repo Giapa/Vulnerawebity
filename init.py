ad208149bce8da128b921f1dec9f2f89d6c96cf5
Gregory Giapa <giapa@protonmail.com>
2020-04-23 15:13:24 +0300

    Update init.py

--- init.py
+++ init.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 class Actions():
 
